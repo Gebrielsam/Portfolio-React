1e47196d67bfb1b524fbb54a0ad8ffd993ab688c
Onah Obinna Samuel <samgb042@gmail.com>
2022-08-20 13:19:40 +0100

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,111 @@
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase, faBars, faClose, } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="samuel" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          onClick={() => setShowNav(false)}
+          exact="true" 
+          activeclassname="active" 
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)} 
+          activeclassname="active" className="about-link" 
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          onClick={() => setShowNav(false)}
+          activeclassname="active" className="portfolio-link" 
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" 
+          />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon'       
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/obinna-samuel-110469231/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Gebrielsam"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCINGHqVAd0zwg87uQqiwkmg"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="https://join.skype.com/invite/OnHBW1k3QXHt" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+
+      <FontAwesomeIcon 
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className='hamburger-icon'
+      />
+    </div>
+  )
+}
+
+export default Sidebar
